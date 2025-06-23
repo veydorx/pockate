f22a1fa7c640b284f5da44999f791aa730149042
veydorx <zeynepbeyner@gmail.com>
2025-06-23 11:48:47 +0300

    Update init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-
 DB_PATH="./pb_data/data.db"
 PORT=${PORT:-3000}
 
 if [ ! -f "$DB_PATH" ]; then
-    echo "Starting fresh DB..."
-    ./pocketbase serve --http=:"$PORT" &
-
-    sleep 3
-
-    echo "Applying PRAGMA & WAL settings..."
-    sqlite3 "$DB_PATH" "PRAGMA journal_mode=WAL;"
-    sqlite3 "$DB_PATH" "PRAGMA synchronous=NORMAL;"
-    sqlite3 "$DB_PATH" "PRAGMA cache_size=10000;"
-    sqlite3 "$DB_PATH" "PRAGMA temp_store=MEMORY;"
-
-    pkill pocketbase
-    sleep 1
+  ./pocketbase serve --http=:"$PORT" &
+  sleep 3
+  sqlite3 "$DB_PATH" "PRAGMA journal_mode=WAL;"
+  sqlite3 "$DB_PATH" "PRAGMA synchronous=NORMAL;"
+  sqlite3 "$DB_PATH" "PRAGMA cache_size=10000;"
+  sqlite3 "$DB_PATH" "PRAGMA temp_store=MEMORY;"
+  pkill pocketbase
+  sleep 1
 fi
 
 exec ./pocketbase serve --http=:"$PORT"
